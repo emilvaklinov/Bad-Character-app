8a92b68f7220f4498377950ae03a59c90efb1105
Emil Vaklinov <emil_vaklinov@yahoo.com>
2020-08-04 00:05:10 +0300

    model set up created

---
+++ Bad Character app/Model/Model.swift
@@ -0,0 +1,21 @@
+//
+//  Model.swift
+//  Bad Character app
+//
+//  Created by Emil Vaklinov on 04/08/2020.
+//  Copyright © 2020 Emil Vaklinov. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+// MARK: - CharacterElement
+struct Character: Decodable {
+    let char_id: Int?
+    let name: String?
+    let occupation: [String]?
+    let img: String?
+    let status: String?
+    let appearance: [Int]?
+}
---
+++ Bad Character app/Parser/Parser.swift
@@ -0,0 +1,21 @@
+//
+//  Parser.swift
+//  Bad Character app
+//
+//  Created by Emil Vaklinov on 04/08/2020.
+//  Copyright © 2020 Emil Vaklinov. All rights reserved.
+//
+
+import Foundation
+
+// Unified parser to parse the data from JSON with T Type. returning T where T is decodable and returning decoded data.
+
+struct JSONParser {
+
+    static func parse<T>(_ data: Data, type: T.Type) throws -> T where T : Decodable {
+
+        return try JSONDecoder().decode(type.self, from: data)
+
+    }
+
+}
